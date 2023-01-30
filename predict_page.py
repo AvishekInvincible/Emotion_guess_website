85e637eee7c98db657950234044082fd2e6403bc
Avishek <87311029+AvishekInvincible@users.noreply.github.com>
2023-01-30 09:51:13 +0000

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -4,12 +4,12 @@ import numpy as np
 
 
 model = load('EmotionalGuess.joblib')
-def show_predict_page():
-    st.title('Emotion Guess :-')
-    st.write("""### Enter the text you want to classify """)
-    sentence = st.text_input('Input your sentence here:') 
+# def show_predict_page():
+#     st.title('Emotion Guess :-')
+#     st.write("""### Enter the text you want to classify """)
+#     sentence = st.text_input('Input your sentence here:') 
 
-    if sentence:
-        st.write("Model's prediction : ",model.predict([sentence])[0])
+#     if sentence:
+#         st.write("Model's prediction : ",model.predict([sentence])[0])
 # conda activate ml
-show_predict_page()
+# show_predict_page()
