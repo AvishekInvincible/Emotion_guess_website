7f6fe1cf87187d85bb9482a8e979887f1eb4a507
Avishek <87311029+AvishekInvincible@users.noreply.github.com>
2023-01-30 10:53:33 +0000

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -1,11 +1,10 @@
 import streamlit as st 
 from joblib import load
 import numpy as np
-data = {'Sentence':[],
-        'Pred':[]}
-df = pd.DataFrame(data)
+
+df = pd.read_csv('data.csv')
 model = load('EmotionalGuess.joblib')
-def show_predict_page():
+def show_predict_page(df):
     st.title('Emotion Guess :-')
     st.write("""### Enter the text you want to classify """)
     sentence = st.text_input('Input your sentence here:') 
@@ -15,11 +14,11 @@ def show_predict_page():
 
     if st.button("Right"):
        df2 = pd.DataFrame([sentence, 1], columns=['Sentence','Pred'])
-       df3 = df1.append(df2)
-       df3.to_csv('Out.csv')
+       df3 = df.append(df2)
+       df3.to_csv('data.csv')
     if st.button("Wrong"):
        df2 = pd.DataFrame([sentence, 0], columns=['Sentence','Pred']) 
-       df3 = df1.append(df2)
-       df3.to_csv('Out.csv')
+       df3 = df.append(df2)
+       df3.to_csv('data.csv')
 # conda activate ml
 show_predict_page()
